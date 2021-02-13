9a9421cb8559cc20b02dec97b1a7a751fead277f
YuxLiu <liuyuxuan1230@outlook.com>
2021-02-13 15:56:17 +0800

    + Card Card2

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -1,21 +1,39 @@
 import React from "react";
-import { Box } from "../components/atomic/Box";
 
 const hold_img = "/images/hold_0.jpg";
 
 export function Card() {
   return (
-    <div className="flex overflow-hidden rounded shadow-2xl max-w-screen-sm">
+    <div className="flex overflow-hidden max-w-sm rounded-lg shadow">
       <div className="relative flex-none w-3/5">
         <img src={hold_img} alt="" className="img-ratio" />
       </div>
-      <form className="flex flex-col flex-auto p-6 h-40 space-y-4">
+      <form className="flex flex-col flex-auto p-6 space-y-6">
         <h1>Title</h1>
         <h2 className="opacity-70">subtitle</h2>
-        <p>see more</p>
+        <span>action</span>
       </form>
     </div>
   );
+}
+
+export function Card2() {
+  return (
+    <div className="flex overflow-hidden flex-col max-w-sm rounded-lg shadow">
+      <div className="relative pb-3/5">
+        <img src={hold_img} alt="" className="img-ratio" />
+      </div>
+      <form className="flex flex-col flex-auto p-6 space-y-2">
+        <h1>Title</h1>
+        <h2 className="opacity-70">subtitle</h2>
+        {/*<span>action</span>*/}
+      </form>
+    </div>
+  );
+}
+
+export function Card3() {
+  return <div className="flex overflow-hidden max-w-sm shadow"></div>;
 }
 
 export default function Dev() {
@@ -23,6 +41,7 @@ export default function Dev() {
     <>
       <div className="px-4">
         <Card />
+        <Card2 />
       </div>
     </>
   );
