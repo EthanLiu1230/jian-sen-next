aa3a5be8e0268be220267efe86e85d1e83e4d28e
YuxLiu <liuyuxuan1230@outlook.com>
2021-02-13 16:23:35 +0800

    + Card

---
+++ components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { hold_img } from "../pages/dev";
+
+export function Card() {
+  return (
+    <div className="flex overflow-hidden max-w-sm rounded-lg shadow sm:flex-col">
+      <div className="relative flex-none w-3/5 sm:relative sm:pb-3/5 sm:w-full sm:flex-1">
+        <img src={hold_img} alt="" className="img-ratio" />
+      </div>
+      <form className="flex flex-col flex-auto p-6 space-y-6 sm:space-y-2">
+        <h1>Title</h1>
+        <h2 className="opacity-70">subtitle</h2>
+        <span className="sm:hidden">action</span>
+      </form>
+    </div>
+  );
+}
--- pages/dev.tsx
+++ pages/dev.tsx
@@ -1,47 +1,13 @@
 import React from "react";
+import { Card } from "../components/Card";
 
-const hold_img = "/images/hold_0.jpg";
-
-export function Card() {
-  return (
-    <div className="flex overflow-hidden max-w-sm rounded-lg shadow">
-      <div className="relative flex-none w-3/5">
-        <img src={hold_img} alt="" className="img-ratio" />
-      </div>
-      <form className="flex flex-col flex-auto p-6 space-y-6">
-        <h1>Title</h1>
-        <h2 className="opacity-70">subtitle</h2>
-        <span>action</span>
-      </form>
-    </div>
-  );
-}
-
-export function Card2() {
-  return (
-    <div className="flex overflow-hidden flex-col max-w-sm rounded-lg shadow">
-      <div className="relative pb-3/5">
-        <img src={hold_img} alt="" className="img-ratio" />
-      </div>
-      <form className="flex flex-col flex-auto p-6 space-y-2">
-        <h1>Title</h1>
-        <h2 className="opacity-70">subtitle</h2>
-        {/*<span>action</span>*/}
-      </form>
-    </div>
-  );
-}
-
-export function Card3() {
-  return <div className="flex overflow-hidden max-w-sm shadow"></div>;
-}
+export const hold_img = "/images/hold_0.jpg";
 
 export default function Dev() {
   return (
     <>
       <div className="px-4">
         <Card />
-        <Card2 />
       </div>
     </>
   );
